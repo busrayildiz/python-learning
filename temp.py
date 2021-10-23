14ecf83f940aa9ee619a9b27e6545dca99f8e341
busrayildiz <bbusrayildizz34@gmail.com>
2021-10-23 16:16:05 +0300

    np.loadtxt test

--- temp.py
+++ temp.py
@@ -1,15 +1,22 @@
 import numpy as np
-import statistics
-import math
-
-a = np.array([1,2,3,4,5,6,7,8,9,10])
 
 
+#import statistics
+#import math
+#
+#a = np.array([1,2,3,4,5,6,7,8,9,10])
+#
+#mean = statistics.mean(a)
+#total = 0
+#for x in a :
+#    total += (x - mean) ** 2 
+#    
+#stdev = math.sqrt(total / len(a) )    
+#print(stdev)
 
-mean = statistics.mean(a)
-total = 0
-for x in a :
-    total += (x - mean) ** 2 
-    
-stdev = math.sqrt(total / len(a) )    
-print(stdev)
+
+
+
+data = np.loadtxt('test.csv', delimiter=',')
+mean=np.mean (data , axis=0)
+print(mean)
