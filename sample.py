9958c2d9bc7012d85532c9e5bcb44ed7b4e791b0
busrayildiz <bbusrayildizz34@gmail.com>
2021-11-16 00:55:17 +0200

    Normalization

--- sample.py
+++ sample.py
@@ -6,6 +6,7 @@ Created on Tue Nov 16 00:17:40 2021
 @author: busrayildiz
 """
 
+"""
 import numpy as np
 
 dataset = np.loadtxt('test2.csv', delimiter=',', skiprows=1)
@@ -19,5 +20,28 @@ def standard_scaler(dataset):
 print(dataset, end='\n\n')
 standard_scaler(dataset)
 print(dataset)        
+"""
+
+
+#NORMALIZATION
+
+import numpy as np
+
+dataset = np.loadtxt('test2.csv', delimiter=',', skiprows=1)
+
+
+def minmax_scaler(dataset):
+    scaled_dataset= np.zeros_like(dataset)
+    for col in range(dataset.shape[1]):
+        #don't use min and max as a variable name because ther are built-in functions names !!
+        minval= (dataset[:,col]).min()
+        maxval= (dataset[:,col]).max()
+
+        scaled_dataset[:, col] =(dataset[:, col] - minval) / (maxval - minval)
+        
+        return scaled_dataset
+        
+scaled_data = minmax_scaler(dataset)        
+print(scaled_data)
         
         
